f250f1480cdd2a47385ee722d72603d8f3b96aa4
qalmaqihir <supine.me@gmail.com>
2022-01-02 13:23:10 +0600

    Coloring problem and Hamiltonian problem added

---
+++ src/Backtracking/ColoringProblem.java
@@ -0,0 +1,56 @@
+package Backtracking;
+
+public class ColoringProblem {
+    private int numOfVertices;
+    private int numOfColors;    // # Chromatic number
+    private int[] colors;
+    private int[][] adjacencyMatrix;
+
+    public ColoringProblem(int[][] adjacencyMatrix, int numOfColors){
+        this.numOfColors= numOfColors;
+        this.adjacencyMatrix=adjacencyMatrix;
+        this.numOfVertices=adjacencyMatrix.length;
+        this.colors=new int[numOfVertices];
+
+    }
+
+    public void solveCP(){
+        if (solveProblem(0)){
+            showResult();
+        }else {
+            System.out.println("No feasible solution....");
+        }
+    }
+
+    private void showResult(){
+        for (int i=0;i<numOfVertices;i++){
+            System.out.println("Node "+(i+1)+ " has the color index: "+ colors[i]);
+        }
+    }
+    private boolean solveProblem(int nodeIndex){
+        if (nodeIndex==numOfVertices){
+            return true;
+        }
+
+        for (int colorIndex=1; colorIndex<numOfColors;colorIndex++){
+            if (isColorValid(nodeIndex,colorIndex)){
+                colors[nodeIndex]=colorIndex;
+                if (solveProblem(nodeIndex+1)){
+                    return true;
+                }
+                //Backtrack
+            }
+        }
+        return false;
+    }
+
+    private boolean isColorValid(int nodeIndex, int colorIndex){
+        for (int i=0;i<numOfVertices;i++){
+            if (adjacencyMatrix[nodeIndex][i]==1 && colorIndex==colors[i]){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
---
+++ src/Backtracking/HamiltonianCycle.java
@@ -0,0 +1,72 @@
+package Backtracking;
+
+public class HamiltonianCycle {
+    private int numOfVertices;
+    private int[] hamiltonianPath;
+    private int[][] adjacencyMatrix;
+
+    public HamiltonianCycle(int [][] adjacencyMatrix){
+        this.adjacencyMatrix = adjacencyMatrix;
+        this.hamiltonianPath = new int[adjacencyMatrix.length];
+        this.numOfVertices= adjacencyMatrix.length;
+
+        this.hamiltonianPath[0]=0;
+
+
+    }
+    public void solveHC(){
+        if(!findFeasibleSolution(1)){
+            System.out.println("No Feasible solution....");
+
+        }else{
+            System.out.println("Showing Hamiltonian Path...");
+            showHamiltonianPath();
+        }
+    }
+
+    private boolean findFeasibleSolution(int position){
+        if (position==numOfVertices){
+            if (adjacencyMatrix[hamiltonianPath[position-1]][hamiltonianPath[0]]==1){
+                return true;
+            }else {
+                return false;
+            }
+        }
+        for (int vertexIndex=1; vertexIndex<numOfVertices;vertexIndex++){
+
+            if(isFeasible(vertexIndex,position)){
+                hamiltonianPath[position]=vertexIndex;
+                if (findFeasibleSolution(position+1)){
+                    return true;
+                }
+                // backtrack
+            }
+        }
+        return false;
+
+    }
+
+    private boolean isFeasible(int vertexIndex, int actualPosition){
+        // first condition: whether two nodes are connected?
+        if(adjacencyMatrix[actualPosition-1][vertexIndex]==0){
+            return false;
+        }
+        // second condition: whether we have checked the node or not
+        for (int i=0;i<actualPosition;i++){
+            if(hamiltonianPath[i]==vertexIndex){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void showHamiltonianPath(){
+        System.out.println("Hamiltonian Cycle: ");
+        for (int i=0; i<hamiltonianPath.length;i++){
+            System.out.print(hamiltonianPath[i]+" ");
+
+        }
+        System.out.print(hamiltonianPath[0]);
+    }
+
+}
